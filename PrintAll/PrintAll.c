4660ab7c4780b672cf2a777b725b933d7ed15cc0
xudede <xubowen5588@163.com>
2020-12-28 13:05:36 +0800

    add PrintAll

---
+++ PrintAll/PrintAll.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char s[], int lim)
+{
+	int c, i, j;
+	j = 0;
+	for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+	{
+		if (i < lim - 2)
+		{
+			s[j] = c;
+			++j;
+
+		}
+	}
+		if (c == '\n')
+		{
+			s[j] = c;
+			++j;
+			++i;
+			s[j] = '\0';
+		}
+		
+		return i;
+	
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+
+}
+int main()
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+	max = 0;
+
+	while ((len = getline(line, MAXLINE)) > 0)
+	{
+		printf("%d,%s", len, line);
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+
+		}
+		if (max > 0)
+	    {
+		printf("%s", longest);
+	    }
+	}
+	
+	return 0;
+
+}
